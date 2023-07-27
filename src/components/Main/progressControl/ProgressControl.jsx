9450b85f61df44adda04115a0ff58796efa69b40
u88872625 <u88872625@gmail.com>
2023-07-27 17:08:44 +0800

    add prev-btn

--- src/components/Main/progressControl/ProgressControl.jsx
+++ src/components/Main/progressControl/ProgressControl.jsx
@@ -1,22 +1,39 @@
 import React from "react";
-import { ReactComponent as IconArrow } from "../../icon/arrow.svg";
+import { ReactComponent as IconNextArrow } from "../../icon/NextArrow.svg";
+import { ReactComponent as IconPrevArrow } from "../../icon/PrevArrow.svg";
 
 function NextBtn({ onClick }) {
   return (
     <React.Fragment>
       <button className="next-btn" onClick={onClick}>
         下一步
-        <IconArrow className="next-arrow" />
+        <IconNextArrow className="next-arrow" />
       </button>
     </React.Fragment>
   );
 }
 
-const ProgressControl = ({ number, data, onClick }) => {
+function PrevBtn({ onClick }) {
+  return (
+    <React.Fragment>
+      <button className="prev-btn" onClick={onClick}>
+        <IconPrevArrow className="prev-arrow" />
+        上一步
+      </button>
+    </React.Fragment>
+  );
+}
+
+const ProgressControl = ({ nowStep, onClick }) => {
   return (
     <div className="progressControl">
-      <div className="btn-container" data-phase={data}>
-        <NextBtn onClick={onClick} />
+      <div className="btn-container">
+        {nowStep > 1 && <PrevBtn onClick={onClick} />}
+        {nowStep < 3 ? (
+          <NextBtn onClick={onClick} />
+        ) : (
+          <button className="checkout-btn">確認訂單</button>
+        )}
       </div>
     </div>
   );
