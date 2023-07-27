28961c8d215b3d6bd9eaa6bd20a306b7262ba773
u88872625 <u88872625@gmail.com>
2023-07-27 17:06:04 +0800

    add input:checked border

--- src/components/Main/step/Step2.jsx
+++ src/components/Main/step/Step2.jsx
@@ -6,23 +6,25 @@ const Step2 = () => {
       <form>
         <h2 className="form-title">運送方式</h2>
         <div className="form-container">
-          <div className="form-radio">
-            <input type="radio" className="input-radio"></input>
+          <label className="form-radio">
+            <input type="radio" name="shipping" className="input-radio"></input>
             <div className="radio-text">
               <div className="text-title">標準運送</div>
               <div className="text-time">約3~7個工作天</div>
             </div>
             <div className="radio-price">免費</div>
-          </div>
+            <div className="radio-input-border"></div>
+          </label>
 
-          <div className="form-radio">
-            <input type="radio"></input>
+          <label className="form-radio">
+            <input type="radio" name="shipping"></input>
             <div className="radio-text">
               <div className="text-title">DHL貨運</div>
               <div className="text-time">48小時內送達</div>
             </div>
             <div className="radio-price">$500</div>
-          </div>
+            <div className="radio-input-border"></div>
+          </label>
         </div>
       </form>
     </div>
